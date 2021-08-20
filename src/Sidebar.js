4ce59189b789edfa09d43e56e435e8f58e5363aa
Shreya Sachdeva <sachdeva.shreya@gmail.com>
2021-08-20 12:06:54 -0700

    changed css style

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,24 @@ const Sidebar = ({ handleHomeBtn }) => {
     return (
         <div className="sidebar">
             <button type="button" onClick={handleHomeBtn}>Home</button>
+            <div>General
+                <p>App</p>
+                <p>E-Commerce</p>
+                <p>Analytics</p>
+                <p>Banking</p>
+                <p>Booking</p>
+            </div>
+            <div>Management
+                <p>User</p>
+                <p>E-Commerce</p>
+                <p>Blog</p>
+            </div>
+            <div>App
+                <p>Mail</p>
+                <p>Chat</p>
+                <p>Calendar</p>
+                <p>Kanban</p>
+            </div>
         </div>
     )
 }
